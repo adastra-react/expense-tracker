f607684e4e5b77722818b4e4e28cd0a8a6ca8660
suneil <kxnglish@gmail.com>
2023-03-08 11:48:00 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem"
+
+const App = () => {
+  return (
+    <div>
+      <h2>React App</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+        <div>Date</div>
+        <div>Title</div>
+        <div>Amount</div>
+    </div>
+  )
+}
+
+export default ExpenseItem
